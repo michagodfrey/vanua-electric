4b1887985cbfc21cc67c2616ecebf09d9a1899e7
Michael Godfrey <m.godfrey59@gmail.com>
2023-02-09 19:56:10 +1200

    removed public folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./docs/public/*.{html,js}"],
+  content: ["./docs/*.{html,js}"],
   theme: {
     height: {
       banner: "400px",
