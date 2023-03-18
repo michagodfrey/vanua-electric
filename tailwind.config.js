a6ee8389571be9dcbddc004d2ac89ced3abba07c
Michael Godfrey <m.godfrey59@gmail.com>
2023-03-18 13:32:51 +1200

    continued styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,23 @@
 module.exports = {
   content: ["./docs/*.{html,js}"],
   theme: {
-    height: {
-      banner: "400px",
-    },
     extend: {
       colors: {
+        "background": "#f7f8f8",
         "storm-gray": "#B0B8B4",
+        "storm-gray-light": "#d8dcda",
         "forest-biome": "#184A45",
+        "forest-biome-light": "#2f5c58",
         "living-coral": "#FC766A",
+        "living-coral-dark": "#e36a5f",
         "logo-color": "#87c0cd",
       },
       backgroundImage: {
         "banner-img": "url('images/savusavu-bay-scaled.jpg')",
       },
       animation: {
-        kenburnsBottom: "kenburnsBottom 5s linear 1.5s both",
+        kenburnsBottom: "kenburnsBottom 5s linear 1s both",
+        fadeIn: "fadeIn 5s linear 1s both",
       },
       keyframes: {
         kenburnsBottom: {
@@ -27,6 +29,14 @@ module.exports = {
           "100%": {
             transform: "scale(1.125) translateY(15px)",
             origin: "bottom",
+          },
+        },
+        fadeIn: {
+          from: {
+            opacity: "0",
+          },
+          to: {
+            opacity: "1",
           },
         },
       },
