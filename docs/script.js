699f6ecf0bb3c4278e4190b692ca077cdb808a67
Michael Godfrey <m.godfrey59@gmail.com>
2023-05-23 14:30:35 +1200

    edits for speed improvement

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,9 @@
 const header = document.querySelector("header");
 
-window.addEventListener("scroll", function () {
+window.addEventListener("scroll", function(e) {
   if (window.pageYOffset > 0) {
     header.classList.add("shadow-lg");
   } else {
     header.classList.remove("shadow-lg");
   }
-});
+}, { passive: true });
